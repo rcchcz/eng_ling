67e9b721ca92a261c210acc66b711691093550cc
rcchcz <rcchcz@gmail.com>
2023-12-09 10:30:34 -0300

    add metodos da tabela de simbolos

--- compilador/tabela_simbolos.c
+++ compilador/tabela_simbolos.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_STRUCTS 100 // Tamanho máximo do array de structs
+
 struct Node {
     char *data;
     struct Node *next;
 };
 
-struct MinhaStruct {
+struct Entidade {
     int id;
     char tipo[50];
     char escopo[50];
@@ -15,6 +17,10 @@ struct MinhaStruct {
     char tipo_retorno[50];
     struct Node *parametros; // Lista ligada de strings para os parâmetros
 };
+
+struct Entidade tabelaDeSimbolos[MAX_STRUCTS]; // Array de structs
+
+int tamanhoAtual = 0; // Variável global para controlar o número atual
 
 // Função para criar um novo nó na lista ligada de strings
 struct Node* criarNo(char *data) {
@@ -25,7 +31,7 @@ struct Node* criarNo(char *data) {
 }
 
 // Função para adicionar um nó à lista ligada de strings
-void adicionarParametro(struct MinhaStruct *estrutura, char *parametro) {
+void adicionarParametro(struct Entidade *estrutura, char *parametro) {
     if (estrutura->parametros == NULL) {
         estrutura->parametros = criarNo(parametro);
     } else {
@@ -48,9 +54,76 @@ void liberarLista(struct Node *head) {
     }
 }
 
+// Função para encontrar o índice da struct no array com base no Id
+int encontrarIndicePorId(int id) {
+    for (int i = 0; i < tamanhoAtual; i++) {
+        if (tabelaDeSimbolos[i].id == id) {
+            return i; // Retorna o índice se o Id for encontrado
+        }
+    }
+    return -1; // Retorna -1 se o Id não for encontrado
+}
+
+// Função para inserir uma nova struct no array
+void inserirStruct(int id, const char *tipo, const char *escopo, _Bool tem_retorno, const char *tipo_retorno) {
+    if (tamanhoAtual < MAX_STRUCTS) {
+        tabelaDeSimbolos[tamanhoAtual].id = id;
+        strncpy(tabelaDeSimbolos[tamanhoAtual].tipo, tipo, sizeof(tabelaDeSimbolos[tamanhoAtual].tipo));
+        strncpy(tabelaDeSimbolos[tamanhoAtual].escopo, escopo, sizeof(tabelaDeSimbolos[tamanhoAtual].escopo));
+        tabelaDeSimbolos[tamanhoAtual].tem_retorno = tem_retorno;
+        strncpy(tabelaDeSimbolos[tamanhoAtual].tipo_retorno, tipo_retorno, sizeof(tabelaDeSimbolos[tamanhoAtual].tipo_retorno));
+        tamanhoAtual++;
+        printf("Sucesso.\n");
+    } else {
+        printf("Falha. Limite atingido.\n");
+    }
+}
+
+// Função para remover uma struct com base no Id
+void removerStruct(int id) {
+    int indice = encontrarIndicePorId(id);
+    if (indice != -1) {
+        // Substitui a struct a ser removida pela última struct no array
+        tabelaDeSimbolos[indice] = tabelaDeSimbolos[tamanhoAtual - 1];
+        tamanhoAtual--;
+        printf("Sucesso.\n");
+    } else {
+        printf("Falha. Id não encontrado.\n");
+    }
+}
+
+// Função para buscar uma struct com base no Id
+void buscarStruct(int id) {
+    int indice = encontrarIndicePorId(id);
+    if (indice != -1) {
+        printf("Id: %d, Tipo: %s, Escopo: %s, Tem Retorno: %d, Tipo de Retorno: %s\n",
+               tabelaDeSimbolos[indice].id,
+               tabelaDeSimbolos[indice].tipo,
+               tabelaDeSimbolos[indice].escopo,
+               tabelaDeSimbolos[indice].tem_retorno,
+               tabelaDeSimbolos[indice].tipo_retorno);
+    } else {
+        printf("Falha. Id não encontrado.\n");
+    }
+}
+
+// Função para atualizar uma struct com base no Id
+void atualizarStruct(int id, const char *novo_tipo, const char *novo_escopo, _Bool novo_tem_retorno, const char *novo_tipo_retorno) {
+    int indice = encontrarIndicePorId(id);
+    if (indice != -1) {
+        strncpy(tabelaDeSimbolos[indice].tipo, novo_tipo, sizeof(tabelaDeSimbolos[indice].tipo));
+        strncpy(tabelaDeSimbolos[indice].escopo, novo_escopo, sizeof(tabelaDeSimbolos[indice].escopo));
+        tabelaDeSimbolos[indice].tem_retorno = novo_tem_retorno;
+        strncpy(tabelaDeSimbolos[indice].tipo_retorno, novo_tipo_retorno, sizeof(tabelaDeSimbolos[indice].tipo_retorno));
+        printf("Sucesso.\n");
+    } else {
+        printf("Falha. Id não encontrado.\n");
+    }
+}
+
 int main() {
     // Exemplo de utilização da estrutura
-    struct MinhaStruct exemplo;
+    struct Entidade exemplo;
     exemplo.id = 1;
     strcpy(exemplo.tipo, "ExemploTipo");
     strcpy(exemplo.escopo, "ExemploEscopo");
@@ -66,7 +139,7 @@ int main() {
     adicionarParametro(&exemplo, "parametro3");
 
     // Exibindo os dados da estrutura e dos parâmetros adicionados
-    printf("ID: %d\n", exemplo.id);
+    printf("Id: %d\n", exemplo.id);
     printf("Tipo: %s\n", exemplo.tipo);
     printf("Escopo: %s\n", exemplo.escopo);
     printf("Tem Retorno: %s\n", exemplo.tem_retorno ? "Sim" : "Não");
@@ -78,6 +151,15 @@ int main() {
         printf("%s\n", atual->data);
         atual = atual->next;
     }
+
+    inserirStruct(1, "Exemplo", "Global", 1, "Inteiro");
+    inserirStruct(2, "OutroExemplo", "Local", 0, "Nenhum");
+    buscarStruct(1); // Busca a struct com Id 1
+    buscarStruct(3); // Tentativa de buscar uma struct com Id inexistente
+    atualizarStruct(1, "NovoTipo", "NovoEscopo", 0, "NovoRetorno");
+    buscarStruct(1); // Verifica se a atualização foi feita corretamente
+    removerStruct(2); // Remove a struct com Id 2
+    buscarStruct(2); // Verifica se a remoção foi feita corretamente
 
     // Liberando a memória alocada para a lista ligada de strings
     liberarLista(exemplo.parametros);
